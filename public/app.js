35104b3cd1640edad00175a9b5a4362b2fee87d9
Samuel Trangmar-Keates <samtkeates@gmail.com>
2020-11-12 19:48:59 +0000

    UI update

--- public/app.js
+++ public/app.js
@@ -1,18 +1,18 @@
 (function() {
     document.getElementById("start").addEventListener('click', (e) => {
         e.stopPropagation()
-        document.getElementById("start").setAttribute('hidden', 'hidden')
-        document.getElementById("buzz").removeAttribute("hidden")
+        document.getElementById("start").classList.add('hide')
+        document.getElementById("buzz").classList.remove("hide")
+
+        this.ws = new WebSocket('ws://' + window.location.host + '/ws')
+        this.ws.addEventListener('message', function(e) {
+            console.log(e)
+            new Audio('/Buzzer-sound.mp3').play()
+        });
+
+        document.getElementById("buzz").addEventListener("click", (e) => {
+            e.stopPropagation()
+            this.ws.send(JSON.stringify({ "user": "default" }))
+        })
     })
-
-    this.ws = new WebSocket('ws://' + window.location.host + '/ws')
-    this.ws.addEventListener('message', function(e) {
-        console.log(e)
-        new Audio('/Buzzer-sound.mp3').play()
-    });
-
-    document.getElementById("buzz").addEventListener("click", function (e) {
-        e.stopPropagation()
-        this.ws.send(JSON.stringify({ "user": "default" }))
-    }.bind(this))
 })()
